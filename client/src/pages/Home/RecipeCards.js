a32ca58d1a22a2041f7271349d598e3e8b71b26e
Steven <le.steven@hotmail.com>
2022-08-19 14:26:37 -0400

    updated recipe card styling

--- client/src/pages/Home/RecipeCards.js
+++ client/src/pages/Home/RecipeCards.js
@@ -2,6 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import {
+  IoRestaurantOutline,
+  IoFastFoodOutline,
+  IoTimerOutline,
+  IoList,
+  IoHappyOutline,
+  IoSadOutline,
+  IoMedalOutline,
+} from "react-icons/io5";
+
 const RecipeCards = ({ filteredList }) => {
   return (
     <Wrapper>
@@ -14,15 +24,62 @@ const RecipeCards = ({ filteredList }) => {
                 <p>{recipe.name}</p>
               </RecipeHeader>
               <RecipeDescription>
-                <p>Ratings: {recipe.userRatings.score}</p>
-                <InfoRow>
-                  <p>Prep: {recipe.prepTimeMinutes} mins</p>
-                  <p>Cook: {recipe.cookTimeMinutes} mins</p>
-                </InfoRow>
-                <InfoRow>
-                  <p>{recipe.nutrition.calories} Calories</p>
-                  <p>{recipe.ingredients.length} Ingredients</p>
-                </InfoRow>
+                {recipe.userRatings.score ? (
+                  recipe.userRatings.score >= 0.9 ? (
+                    <Rating>
+                      <span>
+                        Rating:{" "}
+                        {Math.round(recipe.userRatings.score * 100) + "%"}
+                      </span>
+                      <IoHappyOutline />
+                      <IoMedalOutline />
+                    </Rating>
+                  ) : (
+                    <Rating>
+                      <span>
+                        Rating:{" "}
+                        {Math.round(recipe.userRatings.score * 100) + "%"}
+                      </span>
+                    </Rating>
+                  )
+                ) : (
+                  <Rating>
+                    <span>Rating: N/A</span>
+                    <IoSadOutline />
+                  </Rating>
+                )}
+                <InfoGroup>
+                  <Info>
+                    <IoRestaurantOutline />
+                    {recipe.prepTimeMinutes ? (
+                      <p>Prep: {recipe.prepTimeMinutes} mins</p>
+                    ) : (
+                      <p>Prep: N/A</p>
+                    )}
+                  </Info>
+                  <Info>
+                    <IoTimerOutline />
+                    {recipe.prepTimeMinutes ? (
+                      <p>Cook: {recipe.cookTimeMinutes} mins</p>
+                    ) : (
+                      <p>Cook: N/A</p>
+                    )}
+                  </Info>
+                </InfoGroup>
+                <InfoGroup>
+                  <Info>
+                    <IoFastFoodOutline />
+                    {recipe.nutrition.calories ? (
+                      <p>{recipe.nutrition.calories} Calories</p>
+                    ) : (
+                      <p>N/A Calories</p>
+                    )}
+                  </Info>
+                  <Info>
+                    <IoList />
+                    <p>{recipe.ingredients.length} Ingredients</p>
+                  </Info>
+                </InfoGroup>
               </RecipeDescription>
             </div>
           </Card>
@@ -46,7 +103,7 @@ const Card = styled.div`
   display: flex;
   margin: 2rem;
   padding: 1rem;
-  width: 500px;
+  width: 525px;
   border-radius: var(--border-radius);
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);
   transition: 300ms transform ease-in-out;
@@ -76,19 +133,31 @@ const RecipeDescription = styled.div`
   padding-top: 0.25rem;
 `;
 
-const InfoRow = styled.div`
+const InfoGroup = styled.div`
   display: flex;
-  p {
-    border: 1px grey solid;
-    border-radius: var(--border-radius);
-    padding: 0.5rem 0;
-    width: 9rem;
-    text-align: center;
-    background-color: var(--off-white);
-  }
+  align-items: center;
+  gap: 1rem;
+`;
 
-  p + p {
-    margin-left: 1rem;
+const Rating = styled.div`
+  display: flex;
+
+  > span {
+    padding-right: 0.25rem;
+  }
+`;
+
+const Info = styled.div`
+  display: flex;
+  border: 1px grey solid;
+  border-radius: var(--border-radius);
+  padding: 0.5rem;
+  width: 9rem;
+  background-color: var(--off-white);
+  font-size: 14px;
+
+  > p {
+    padding-left: 0.5rem;
   }
 `;
 
