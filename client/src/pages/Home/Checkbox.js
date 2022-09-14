df80a598ca0cb8187bb28b4d54f70c7b1b9d36d3
Steven <le.steven@hotmail.com>
2022-09-13 23:27:35 -0400

    css to checkbox filters

--- client/src/pages/Home/Checkbox.js
+++ client/src/pages/Home/Checkbox.js
@@ -1,9 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const labels = {
   Protein: ["Chicken", "Pork", "Beef", "Tofu", "Egg"],
-  Toppings: ["Green Onions", "Garlic", "Mushroom", "Ginger", "Carrot"],
+  Toppings: ["Onions", "Garlic", "Mushroom", "Ginger", "Carrot"],
   Others: ["Soy Sauce", "Fish Sauce", "Sesame Oil", "Rice Vinegar", "Sriracha"],
 };
 
@@ -12,20 +12,24 @@ const labelKeys = Object.keys(labels);
 const Checkbox = ({ handleToggle, checkFilters }) => {
   return (
     <Wrapper>
-      {labelKeys.map((labelKey, index) => (
-        <Option key={`${labelKey}-${index + 1}`}>
+      {labelKeys.map((labelKey) => (
+        <Option key={labelKey}>
           <h1>{labelKey}</h1>
-          {labels[labelKey].map((ingredient, index) => (
-            <div key={`${ingredient}-${index + 1}`}>
-              <input
-                type="checkbox"
-                id={ingredient}
-                value={ingredient}
-                onChange={handleToggle}
-                checked={checkFilters.indexOf(ingredient) === -1 ? false : true}
-              />
-              <label htmlFor={ingredient}>{ingredient}</label>
-            </div>
+          {labels[labelKey].map((ingredient) => (
+            <StyledLabel
+              htmlFor={ingredient}
+              checked={checkFilters.indexOf(ingredient) === -1 ? false : true}
+            >
+              <Ingredient key={ingredient}>
+                {ingredient}
+                <StyledInput
+                  type="checkbox"
+                  id={ingredient}
+                  value={ingredient}
+                  onChange={handleToggle}
+                />
+              </Ingredient>
+            </StyledLabel>
           ))}
         </Option>
       ))}
@@ -33,28 +37,66 @@ const Checkbox = ({ handleToggle, checkFilters }) => {
   );
 };
 
+const fadeInAnimation = keyframes`
+0% {opacity: 0}
+100% {opacity: 1}
+`;
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  margin: 10px 0 25px 0;
-
+  animation: ${fadeInAnimation} 1s;
   div {
     padding: 0.25rem;
   }
 `;
 
 const Option = styled.div`
-  width: 150px;
   border-radius: 10px;
   font-size: 16px;
   font-family: var(--font-body);
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 150px;
+  gap: 0.5rem;
+`;
 
-  label {
-    padding-left: 0.5rem;
+const Ingredient = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+
+  :hover {
+    transform: scale(1.05);
+  }
+`;
+
+const StyledLabel = styled.label`
+  text-align: center;
+  border: 0.1px solid grey;
+  border-radius: 5px;
+  background: ${({ checked }) => (checked ? "var(--blue)" : "")};
+  color: ${({ checked }) => (checked ? "white" : "")};
+  transition: background 0.1s linear, color 0.1s linear;
+
+  :hover {
+    cursor: pointer;
+    background: ${({ checked }) =>
+      checked ? "var(--blue)" : "var(--off-white)"};
+    transition: background 0.1s linear;
+  }
+`;
+
+const CheckBoxPop = keyframes`
+0% {opacity: 0;}
+75% {opacity: 1;}
+100% {transform: scale(1.5)}
+`;
+
+const StyledInput = styled.input`
+  :checked {
+    animation: ${CheckBoxPop} 0.2s;
   }
 `;
 
